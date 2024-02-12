40b0256a2e156032187995636bd12a6eb53c0644
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-02-12 12:20:25 +0800

    rating fixed

--- src/index.js
+++ src/index.js
@@ -131,7 +131,10 @@ function Rating({ rating }) {
                     ${rating <= 7.5 && rating >= 5.1 && "good"}
                     ${rating <= 10 && rating >= 7.6 && "very-good"}`}
     >
-      sample
+      {rating <= 2.5 && "Very-bad"}
+      {rating <= 5 && rating >= 2.6 && "Bad"}
+      {rating <= 7.5 && rating >= 5.1 && "Good"}
+      {rating <= 10 && rating >= 7.6 && "Very-good"}
     </span>
   );
 }
