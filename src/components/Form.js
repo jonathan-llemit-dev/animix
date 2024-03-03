6356eb6709b6068341ae9663a5ed1f368af64e5d
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-03-03 21:59:46 +0800

    added form and inputs on add new anime section

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,6 +8,12 @@ export default function Form() {
         <p>
           Anyone can contribute to our community, submit a new list of anime.
         </p>
+        <form>
+          <input type="text" name="title" />
+          <input type="text" name="genre" />
+          <input type="text" name="rating" />
+          <input type="text" name="summary" />
+        </form>
       </div>
     </main>
   );
