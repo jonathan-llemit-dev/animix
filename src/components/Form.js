3e46a586b54fbe726f1a437b7646dbcb61acd533
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-03-12 21:13:32 +0800

    alignment on form inputs modified

--- src/components/Form.js
+++ src/components/Form.js
@@ -8,20 +8,28 @@ export default function Form() {
         <p>
           Anyone can contribute to our community, submit a new list of anime.
         </p>
-        <form>
+      </div>
+      <form>
+        <div>
           <label for="title">Title:</label>
           <input type="text" id="title" name="title" />
+        </div>
 
+        <div>
           <label for="genre">Genre:</label>
           <input type="text" id="genre" name="genre" />
+        </div>
 
+        <div>
           <label for="rating">Rating:</label>
           <input type="text" id="rating" name="rating" />
+        </div>
 
+        <div>
           <label for="summary">Summary:</label>
           <input type="text" id="summary" name="summary" />
-        </form>
-      </div>
+        </div>
+      </form>
     </main>
   );
 }
